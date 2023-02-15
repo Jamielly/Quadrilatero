1749792333550ef004996d2899a080ef336d9df3
Jamielly <jamielly.reis@fatec.sp.gov.br>
2023-02-15 15:25:38 -0300

    Métodos

---
+++ Quadriculado/src/Ex_2/Principal.java
@@ -0,0 +1,16 @@
+package Ex_2;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+
+        // Quadrilátero
+        System.out.println("Exercício quadrilátero");
+        Quadri.area(3);
+        Quadri.area(5d,5d);
+        Quadri.area(7,8,9);
+        Quadri.area(5f,5f);
+
+    }
+}
+
